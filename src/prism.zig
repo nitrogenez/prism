4054190c6cc91aaab45fe10e708ea0894d0d1ce1
nitrogenez <nitrogenesis.developer@gmail.com>
2023-08-21 18:02:21 +0300

    renamed index file, added public imports

---
+++ src/prism.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+
+pub const colors = @import("colors.zig");
+
+pub const RGB = @import("rgb.zig").RGB;
+pub const HSL = @import("hsl.zig").HSL;
+pub const HSV = @import("hsv.zig").HSV;
+pub const YIQ = @import("yiq.zig").YIQ;
