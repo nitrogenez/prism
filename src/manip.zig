3fb25ebba0d4a8f62cc79b5d5ed97cc87afc1e91
nitrogenez <nitrogenez.dev@tuta.io>
2024-09-07 22:22:46 +0300

    feat(manip): new manipulations

--- src/manip.zig
+++ src/manip.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const prism = @import("root.zig");
 
 /// Lightens (tints) the color by `factor`
 pub fn tint(rgb: [3]f64, factor: f64) [3]f64 {
@@ -12,4 +13,38 @@ pub fn tint(rgb: [3]f64, factor: f64) [3]f64 {
 /// Darkens (shades) the color by `factor`
 pub fn shade(rgb: [3]f64, factor: f64) [3]f64 {
     return .{ rgb[0] * (1 - factor), rgb[1] * (1 - factor), rgb[2] * (1 - factor) };
+}
+
+/// Blends two colors together at a certain point t
+pub fn blendAt(rgb_rhs: [3]f64, rgb_lhs: [3]f64, t: f64) [3]f64 {
+    return [3]f64{
+        @sqrt((1.0 - t) * (rgb_rhs[0] * rgb_rhs[0]) + t * (rgb_lhs[0] * rgb_lhs[0])),
+        @sqrt((1.0 - t) * (rgb_rhs[1] * rgb_rhs[1]) + t * (rgb_lhs[1] * rgb_lhs[1])),
+        @sqrt((1.0 - t) * (rgb_rhs[2] * rgb_rhs[2]) + t * (rgb_lhs[2] * rgb_lhs[2])),
+    };
+}
+
+/// Blends two colors
+pub fn blend(rgb_rhs: [3]f64, rgb_lhs: [3]f64) [3]f64 {
+    return [3]f64{
+        0.5 * rgb_rhs[0] + 0.5 * rgb_lhs[0],
+        0.5 * rgb_rhs[1] + 0.5 * rgb_lhs[1],
+        0.5 * rgb_rhs[2] + 0.5 * rgb_lhs[2],
+    };
+}
+
+/// Saturates the color. RGB values must be normalized. `amount` = 0...1
+pub fn saturate(rgb: [3]f64, amount: f64) [3]f64 {
+    var hsl = prism.rgbToHsl(rgb);
+    hsl[1] += amount;
+    hsl[1] = std.math.clamp(hsl[1], 0.0, 1.0);
+    return prism.hslToRgb(hsl);
+}
+
+/// Desaturates the color. RGB values must be normalized. `amount` = 0...1
+pub fn desaturate(rgb: [3]f64, amount: f64) [3]f64 {
+    var hsl = prism.rgbToHsl(rgb);
+    hsl[1] -= amount;
+    hsl[1] = std.math.clamp(hsl[1], 0.0, 1.0);
+    return prism.hslToRgb(hsl);
 }
